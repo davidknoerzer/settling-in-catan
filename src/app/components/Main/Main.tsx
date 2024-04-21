3c5a1cd5bb075fba8603a64104144f440418cfd4
David Knörzer <knoerzerdavid@gmail.com>
2024-04-21 21:54:45 +0200

    lint issue solved

--- src/app/components/Main/Main.tsx
+++ src/app/components/Main/Main.tsx
@@ -90,8 +90,12 @@ export default function Main() {
       <div className="navbar bg-base-100">
         <div className="navbar-start">
           <h1 className="text-3xl p-1">Settling in Catan</h1>
-          <button className="btn btn-circle" onClick={()=>document.getElementById('info_modal').showModal()} title="Info">
-          <svg className="h-6 w-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <button className="btn btn-circle" onClick={() => {
+            const modal = document.getElementById('info_modal') as HTMLDialogElement;
+            if (modal) {
+              modal.showModal();
+            }
+          }} title="Info">          <svg className="h-6 w-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <circle cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="1.5"/>
             <path d="M12 17V11" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round"/>
             <circle cx="1" cy="1" r="1" transform="matrix(1 0 0 -1 11 9)" fill="currentColor"/>
